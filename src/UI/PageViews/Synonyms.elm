fa8dcef18228399332cb2f0de517072167ee3a55
Sufiyan <sufiyanyusuf78611@gmail.com>
2022-06-03 21:41:04 +0300

    pre req track commit

--- src/UI/PageViews/Synonyms.elm
+++ src/UI/PageViews/Synonyms.elm
@@ -1,6 +1,7 @@
 module UI.PageViews.Synonyms exposing (..)
 
 import Element exposing (..)
+import Json.Decode exposing (Error(..))
 import UI.Styles
 
 
@@ -13,3 +14,18 @@ view =
     el
         (UI.Styles.getTypographicStyleFor UI.Styles.H1)
         (text "(Views.pageTitle Views.Tasks)")
+
+
+type RequestStatus
+    = None
+    | Loading
+    | Success
+    | Failure
+
+
+type alias CardModel =
+    { status : RequestStatus
+    , taskID : Maybe Int
+    , title : String
+    , values : List String
+    }
